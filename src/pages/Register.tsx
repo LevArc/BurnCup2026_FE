8441ad2d03972b1fde8c454a10218fdefbb63bf4
Nabhan <nabhan0816@gmail.com>
2026-07-16 15:55:44 +0700

    Add login and register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,10 @@
+import '../components/Register/Register.css';
+import Register from '../components/Register/Register';
+
+export default function RegisterPage() {
+  return (
+    <>
+      <Register />
+    </>
+  );
+}
